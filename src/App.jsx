4b46aad9fddcad62807b123ba592945a7327cb97
JT <jstorbit@gmail.com>
2025-12-11 18:08:38 -0700

    Add GuestItem component and base, add base App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import GuestItem from "./GuestItem";
 
 const App = () => {
-  
+  const [guests, setGuests] = useState([]);
 
   useEffect(() => {
     const getGuests = async () => {
@@ -9,12 +10,19 @@ const App = () => {
       const response = await fetch(API);
       const dataAccess = await response.json();
       const retrievedGuests = dataAccess.data
-      console.log(retrievedGuests);
+      setGuests(retrievedGuests);
     }
     getGuests()
   }, [])
   
-  return <></>;
+  return (
+    <>
+      <h1>Guests for the Party</h1>
+      <ul>
+        {guests.map((guest) => <GuestItem key={guest.id} guest={guest} />)}
+      </ul>
+    </>
+  );
 }
 
 export default App;
---
+++ src/GuestItem.jsx
@@ -0,0 +1,5 @@
+const GuestItem = () => {
+
+}
+
+export default GuestItem;
