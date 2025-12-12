3a5fbe7873132c83676daece4ae3865f65fc5b33
JT <jstorbit@gmail.com>
2025-12-11 17:55:03 -0700

    Add useEffect to fetch users

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
-export default function App() {
+import { useEffect } from "react";
+
+const App = () => {
+  
+
+  useEffect(() => {
+    const getGuests = async () => {
+      const API = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2510-FTB-CT-WEB-PT/guests";
+      const response = await fetch(API);
+      const dataAccess = await response.json();
+      const retrievedGuests = dataAccess.data
+      console.log(retrievedGuests);
+    }
+    getGuests()
+  }, [])
+  
   return <></>;
 }
+
+export default App;
